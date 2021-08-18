05218c0bf832cac1b1c7e72dce44d683b2924ac4
Alexander Hangl <44925346+ahangl@users.noreply.github.com>
2021-08-18 11:03:13 +0200

    feat: create basic ui5 custom task

--- packages/ui5-task-babel/lib/ui5-task-babel.js
+++ packages/ui5-task-babel/lib/ui5-task-babel.js
@@ -1,7 +1,18 @@
 'use strict';
 
-module.exports = ui5TaskBabel;
-
-function ui5TaskBabel() {
-    // TODO
-}
+/**
+ * Custom task example
+ *
+ * @param {object} parameters Parameters
+ * @param {module:@ui5/fs.DuplexCollection} parameters.workspace DuplexCollection to read and write files
+ * @param {module:@ui5/fs.AbstractReader} parameters.dependencies Reader or Collection to read dependency files
+ * @param {object} parameters.taskUtil Specification Version dependent interface to a [TaskUtil]{@link module:@ui5/builder.tasks.TaskUtil} instance
+ * @param {object} parameters.options Options
+ * @param {string} parameters.options.projectName Project name
+ * @param {string} [parameters.options.projectNamespace] Project namespace if available
+ * @param {string} [parameters.options.configuration] Task configuration if given in ui5.yaml
+ * @returns {Promise<undefined>} Promise resolving with <code>undefined</code> once data has been written
+ */
+module.exports = async function ({ workspace, dependencies, taskUtil, options }) {
+    // [...]
+};
