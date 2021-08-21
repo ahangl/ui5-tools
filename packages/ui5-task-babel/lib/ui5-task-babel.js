8a884dd94da1bb216b3996bb02243a306bace7c9
Alexander Hangl <44925346+ahangl@users.noreply.github.com>
2021-08-21 23:12:04 +0200

    feat: reduce the ui5 specification version of the task

--- packages/ui5-task-babel/lib/ui5-task-babel.js
+++ packages/ui5-task-babel/lib/ui5-task-babel.js
@@ -10,14 +10,13 @@ const log = require('@ui5/logger').getLogger('builder:customtask:babel')
  * @param {object} parameters Parameters
  * @param {module:@ui5/fs.DuplexCollection} parameters.workspace DuplexCollection to read and write files
  * @param {module:@ui5/fs.AbstractReader} parameters.dependencies Reader or Collection to read dependency files
- * @param {object} parameters.taskUtil Specification Version dependent interface to a [TaskUtil]{@link module:@ui5/builder.tasks.TaskUtil} instance
  * @param {object} parameters.options Options
  * @param {string} parameters.options.projectName Project name
  * @param {string} [parameters.options.projectNamespace] Project namespace if available
  * @param {string} [parameters.options.configuration] Task configuration if given in ui5.yaml
  * @returns {Promise<undefined>} Promise resolving with <code>undefined</code> once data has been written
  */
-module.exports = async function ({ workspace, dependencies, taskUtil, options }) {
+module.exports = async function ({ workspace, dependencies, options }) {
     let resources = await workspace.byGlob('/**/*.js')
 
     const babelConfig = {
